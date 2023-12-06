ea567ae7c3c55076492a7ef3333d299ad33a39f4
Ponrawoot <minnaja853@gmail.com>
2023-12-06 21:32:45 +0700

    tailwind config - font family, font size, and color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary': '#2A4D69',
+        'secondary': '#4B86B4',
+        'thrid': '#1890FF',
+        'background': '#E7EFF6',
+        'darker-background': '#D1E2EF',
+      },
+      fontFamily: {
+        default : ['Noto Sans Thai', 'sans-serif']
+      },
+      fontSize: {
+        'h1': '38px',
+        'h2': '30px',
+        'h3': '24px',
+        'h4': '20px',
+        'h5': '16px',
+        'body': '14px',
+        'description': '12px',
+        'h1-mobile': '24px',
+        'h2-mobile': '16px',
+        'h3-mobile': '12px',
+        'h4-mobile': '10px',
+        'h5-mobile': '8px',
+        'body-mobile': '6px',
+        'description-mobile': '4px'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -15,6 +41,7 @@ const config: Config = {
       },
     },
   },
+
   plugins: [],
 }
 export default config
