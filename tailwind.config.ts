7c7e9c3db3653c52a8605511bdef5bb240a423b9
oatRatchanon <88995860+oatRatchanon@users.noreply.github.com>
2023-12-19 15:17:29 +0700

    initial landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,42 +1,45 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'primary': '#2A4D69',
-        'secondary': '#4B86B4',
-        'thrid': '#1890FF',
-        'background': '#E7EFF6',
-        'darker-background': '#D1E2EF',
+        primary: "#2A4D69",
+        secondary: "#4B86B4",
+        thrid: "#1890FF",
+        background: "#E7EFF6",
+        "darker-background": "#D1E2EF",
       },
       fontFamily: {
-        default : ['Noto Sans Thai', 'sans-serif']
+        default: ["Noto Sans Thai", "sans-serif"],
       },
       fontSize: {
-        'h1': '38px',
-        'h2': '30px',
-        'h3': '24px',
-        'h4': '20px',
-        'h5': '16px',
-        'body': '14px',
-        'description': '12px',
-        'h1-mobile': '24px',
-        'h2-mobile': '16px',
-        'h3-mobile': '12px',
-        'h4-mobile': '10px',
-        'h5-mobile': '8px',
-        'body-mobile': '6px',
-        'description-mobile': '4px'
+        h1: "38px",
+        h2: "30px",
+        h3: "24px",
+        h4: "20px",
+        h5: "16px",
+        body: "14px",
+        description: "12px",
+        "h1-mobile": "24px",
+        "h2-mobile": "16px",
+        "h3-mobile": "12px",
+        "h4-mobile": "10px",
+        "h5-mobile": "8px",
+        "body-mobile": "6px",
+        "description-mobile": "4px",
+      },
+      screens: {
+        mobile: { max: "394px" },
       },
     },
   },
 
   plugins: [],
-}
-export default config
+};
+export default config;
