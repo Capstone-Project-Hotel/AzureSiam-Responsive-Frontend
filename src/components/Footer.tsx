447a55d5e262458ad27fc55faf7f52eda53c0834
oatRatchanon <88995860+oatRatchanon@users.noreply.github.com>
2024-01-30 23:30:30 +0700

    fix footer problem

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function Footer({ t }: { t: any }) {
   return (
     <div
-      className="flex flex-row w-full h-[18vh] px-[4.16vw] items-center justify-between"
+      className="flex flex-row w-full h-[170px] px-[4.16vw] items-center justify-between"
       style={{ backgroundColor: "#2A4D69", color: "white", overflow: "hidden" }}
     >
       <div className="flex flex-row items-center">
         <BlockOutlined style={{ fontSize: "8vh", color: "white" }} />
-        <div className="flex flex-col px-[2.08vw]">
+        <div className="flex flex-col px-[2.08vw] gap-y-1">
           <p className="text-h3 font-regular mobile:text-h3-mobile">
             {" "}
             {t("hotel")}{" "}
