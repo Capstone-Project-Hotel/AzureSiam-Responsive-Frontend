94f591ddb387453af108efc31e0aa5118f0aa287
oatRatchanon <88995860+oatRatchanon@users.noreply.github.com>
2024-02-27 18:42:12 +0700

    update google map

--- src/app/[lng]/contact-us/page.tsx
+++ src/app/[lng]/contact-us/page.tsx
@@ -49,12 +49,28 @@ export default function ContactUs({
             </div>
           </div>
         </div>
-        <div className="flex flex-row mt-[1.5vw] items-center justify-center">
-          <img
-            src={"https://via.placeholder.com/800x300"}
-            alt="Google Map Mock"
-            className="w-[55vw] h-[19vw]"
-          />
+        <div className="flex flex-row mt-[1.5vw] items-center justify-center px-5">
+          {lng == "th" ? (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.6625687957862!2d100.52583277589487!3d13.738867597591229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29f2aae33623f%3A0x421e0643a63c2093!2z4LiI4Li44Lis4Liy4Lil4LiH4LiB4Lij4LiT4LmM4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii!5e0!3m2!1sth!2sth!4v1709033731764!5m2!1sth!2sth"
+              width="800"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          ) : (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.6625687957862!2d100.52583277589487!3d13.738867597591229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29f2aae33623f%3A0x421e0643a63c2093!2sChulalongkorn%20University!5e0!3m2!1sen!2sth!4v1709033933904!5m2!1sen!2sth"
+              width="800"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          )}
         </div>
       </div>
       <Footer t={t} />
